82ea16eba7d83e97f1f9a705205d5d30f6458cae
Octavian Rusu <octavian.concept@gmail.com>
2023-01-17 15:44:52 -0700

    transfer code to jupyter notebook for quick modification

--- model.py
+++ model.py
@@ -5,4 +5,6 @@ import pandas as pd
 # Setup torch
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-raw_data = pd.read_csv('animes.csv')
+# Setup dataset
+raw_data = pd.read_csv('./animes.csv')
+raw_data = list(raw_data['synopsis'])
